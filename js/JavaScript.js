ae2ae630268196b20507972ded75d5967065b970
Victor Espinoza <vic.espinozac@gmail.com>
2017-03-21 01:16:26 -0700

    First Commit

---
+++ js/JavaScript.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Victor Espinoza on 30/11/2015.
+ */
+$(document).ready(function () {
+
+    $('button').click(function () {
+
+        $('.parent').append("<div class='alert alert-success alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>Success! message sent successfully.</div>")
+
+
+
+    })
+
+});
